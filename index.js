c39ed866d75db372c10274d248b4ed9d1063d992
Vitvita222 <115868179+Vitvita222@users.noreply.github.com>
2022-11-06 13:41:56 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,3 +4,35 @@ const header = document.querySelector('.header')
 darkmode.addEventListener('click', () => {
     
 })
+
+const aboutbtn = document.querySelector('.about')
+
+function smoothScroll(target,duration){
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+    function animation(currentTime){
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime
+        var run = ease(timeElapsed,startPosition,distance,duration);
+        window.scrollTo(0,run);
+        if(timeElapsed < duration) requestAnimationFrame(animation)
+    }
+    function ease(t,b,c,d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t* (t - 2) - 1) +b;
+    }
+    
+
+    requestAnimationFrame(animation)
+}
+    
+var button1 = document.querySelector('.about')
+
+button1.addEventListener('click', () => {
+    smoothScroll('.aboutme',1000);
+})
